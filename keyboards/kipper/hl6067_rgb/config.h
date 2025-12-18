cab48c310047e26e108193c89a0fea6a351af97e
Kipper Sun <128725496+KipperSun@users.noreply.github.com>
2025-12-18 23:20:16 +0800

    Selfmade keyboard of kbd67_ble

---
+++ keyboards/kipper/hl6067_rgb/config.h
@@ -0,0 +1,70 @@
+/* Copyright 2024 keymagichorse
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+
+#ifndef SERIAL_NUMBER
+#   define SERIAL_NUMBER "mjl_hl6095"
+#endif
+
+/*******************************************************matrix********************************************************/
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { A8, A9, A10, B10, B9 }
+#define MATRIX_COL_PINS { A3, A2, A1, A7, B0, B15, B8, B14, B13, B12, B1, B3, A15, A14, A13 }
+
+/*******************************************************matrix********************************************************/
+
+#ifdef BLUETOOTH_BHQ
+// Its active level is "BHQ_IRQ_AND_INT_LEVEL of bhq.h "
+#   define BHQ_INT_PIN          B4
+#   define BHQ_IQR_PIN          B5
+
+#   define USB_POWER_SENSE_PIN  A5             // USB插入检测引脚
+#   define USB_POWER_CONNECTED_LEVEL   1
+
+#   define UART_DRIVER          SD1
+#   define UART_TX_PIN          B6
+#   define UART_TX_PAL_MODE     7
+#   define UART_RX_PIN          B7
+#   define UART_RX_PAL_MODE	    7
+
+// STM32使用到的高速晶振引脚号，做低功耗需要用户配置，每款芯片有可能不一样的
+#define LPM_STM32_HSE_PIN_IN     H0
+#define LPM_STM32_HSE_PIN_OUT    H1
+
+#define REPORT_BUFFER_QUEUE_SIZE    68
+#define BATTER_ADC_PIN              A6
+#define BATTER_ADC_DRIVER           ADCD1
+
+#endif
+
+#define WS2812_POWER_PIN    A4
+#define WS2812_BYTE_ORDER   WS2812_BYTE_ORDER_GRB
+#define WS2812_POWER_ON_LEVEL   1
+
+
+
+#    define WS2812_PWM_DRIVER PWMD5  // default: PWMD2
+#    define WS2812_PWM_CHANNEL 1  // default: 2
+#    define WS2812_PWM_PAL_MODE 1  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
+#    define WS2812_DMA_STREAM STM32_DMA1_STREAM5  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#    define WS2812_DMA_CHANNEL 3  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
---
+++ keyboards/kipper/hl6067_rgb/hl6078_rgb.c
@@ -0,0 +1,94 @@
+/* Copyright 2024 keymagichorse
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "quantum.h"
+
+#if defined(BLUETOOTH_BHQ)
+#   include "bhq.h"
+#   include "bhq_common.h"
+#endif
+
+#if defined(KB_LPM_ENABLED)
+#   include "lpm.h"
+#endif
+
+#if defined(KB_DEBUG)
+#   include "km_printf.h"
+#endif
+#include "battery.h"
+
+
+
+void board_init(void)
+{
+#if defined(BLUETOOTH_BHQ)
+    bhq_common_init();
+#   if defined(KB_LPM_ENABLED)
+    lpm_init();
+#   endif
+#endif
+
+#   if defined(KB_DEBUG)
+    km_printf_init();
+    km_printf("hello rtt log1111111\r\n");
+#   endif
+}
+void housekeeping_task_kb(void) {
+#if defined(BLUETOOTH_BHQ)
+    bhq_wireless_task();
+    #   if defined(KB_LPM_ENABLED)
+        lpm_task();
+    #   endif
+#endif
+}
+
+
+#ifdef RGB_MATRIX_ENABLE
+
+led_config_t g_led_config = {
+	{
+    {     54,     55,     56,     57,     58,     59,     60,     61,     62,     63,     64,     65,     66,     67,     68 },
+		{     53,     52,     51,     50,     49,     48,     47,     46,     45,     44,     43,     42,     41,     40,     39 },
+		{     25,     26,     27,     28,     29,     30,     31,     32,     33,     34,     35,     36, NO_LED,     37,     38 },
+		{     24, NO_LED,     23,     22,     21,     20,     19,     18,     17,     16,     15,     14,     13,     12,     11 },
+		{      0,      1,      2,      3, NO_LED, NO_LED,      4, NO_LED, NO_LED,      5,      6,      7,      8,      9,     10 }
+	},
+    // LED Index to Physical Position
+    {
+        // Row 0: 15 keys
+        { 7.00, 0}, {21.00, 0}, {35.00, 0}, {49.00, 0}, {63.00, 0}, { 77.00, 0}, { 91.00, 0}, {105.00, 0}, {119.00, 0}, {133.00, 0}, {147.00, 0}, {161.00, 0}, {175.00, 0}, {196.00, 0}, {217.00, 0},
+
+        // Row 1: 15 keys
+        {10.50,16}, {28.00,16}, {42.00,16}, {56.00,16}, {70.00,16}, { 84.00,16}, { 98.00,16}, {112.00,16}, {126.00,16}, {140.00,16}, {154.00,16}, {168.00,16}, {182.00,16}, {199.50,16}, {217.00,16},
+
+        // Row 2: 14 keys
+        {12.25,32}, {31.50,32}, {45.50,32}, {59.50,32}, {73.50,32}, { 87.50,32}, {101.50,32}, {115.50,32}, {129.50,32}, {143.50,32}, {157.50,32}, {171.50,32},              {194.25,32}, {217.00,32},
+
+        // Row 3: 14 keys
+        {15.75,48},             {38.50,48}, {52.50,48}, {66.50,48}, { 80.50,48}, { 94.50,48}, {108.50,48}, {122.50,48}, {136.50,48}, {150.50,48}, {164.50,48}, {183.75,48}, {203.00,48}, {217.00,48},
+
+        // Row 4: 11 keys
+        { 8.75,64}, {26.25,64}, {43.75,64}, {61.25,64},                          { 96.25,64},                           {131.25,64}, {148.75,64}, {166.25,64}, {189.00,64}, {203.00,48}, {217.00,64}
+    },
+    {
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,      // 15
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,      // 15
+		8, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,         // 14 第一个是大写灯
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,         // 14
+		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4                   // 11
+	}
+};
+
+#endif
