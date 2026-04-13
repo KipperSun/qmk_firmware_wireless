068464f006612528ef574eb3f404228b7c694cd8
KeyDreamLin <57210347+LinKeyDream@users.noreply.github.com>
2026-04-13 23:33:56 +0800

    hl6078降低功耗到38ua

--- keyboards/keymagichorse/hl6078_rgb/config.h
+++ keyboards/keymagichorse/hl6078_rgb/config.h
@@ -93,10 +93,12 @@
 #define WS2812_POWER_ON_LEVEL   1  
 
 #define RGB_MATRIX_LED_FLUSH_LIMIT 30
+#define WS2812_TRST_US      380
 #define WS2812_BYTE_ORDER   WS2812_BYTE_ORDER_GRB
+
 #define WS2812_PWM_DRIVER   PWMD3  // default: PWMD2
 #define WS2812_PWM_CHANNEL  2  // default: 2
 #define WS2812_PWM_PAL_MODE 2  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
 #define WS2812_DMA_STREAM   STM32_DMA1_STREAM2  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
 #define WS2812_DMA_CHANNEL  5  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
-#define WS2812_TRST_US      380
+
